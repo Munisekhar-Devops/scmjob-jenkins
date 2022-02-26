e60d9d2dbf524ffe52723cb917b4133825ed779b
Munisekhar-Devops <mmunisekhar@gmail.com>
2022-02-26 18:41:01 +0530

    code changed

--- testscript.sh
+++ testscript.sh
@@ -3,3 +3,4 @@ NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "Your Numberone value is" $NUMBERONE
 echo "Your Numbertwo value is" $NUMBERTWO
 echo "Your Total value is ", "$NUMBER"
+echo "This job was completed Successfully"
